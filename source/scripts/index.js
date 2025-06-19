ff985e383b9bd9892deef7f1c9ce96108355d4f0
Borness <mercuryyq2@gmail.com>
2025-06-19 17:32:12 +0300

    1

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,4 @@
-/* в этот файл добавляет скрипты*/
+document.querySelector('.contacts').classList.remove('contacts--nojs');
+
+const menuToggle = document.querySelector('.navigation__toggle');
+const navigationList = document.querySelector('.navigation__list');
