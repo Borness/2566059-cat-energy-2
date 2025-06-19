d2d246c48873a1ba95da26ea6ea73b76734cf2f6
Borness <mercuryyq2@gmail.com>
2025-06-19 18:19:21 +0300

    3

--- source/scripts/index.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.contacts').classList.remove('contacts--nojs');
-
-const menuToggle = document.querySelector('.navigation__toggle');
-const navigationList = document.querySelector('.navigation__list');
